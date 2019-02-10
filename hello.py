6eb7cbe42f323eaf81437720fca7349d99b2fa28
Weston Harper <wes@tao.team>
2019-02-10 11:25:23 -0700

    feat(hello.py): Request and print a list of valid instruments

--- hello.py
+++ hello.py
@@ -1,57 +1,22 @@
-import configparser 
-import oandapy as opy
+import configparser
+import json
 import pandas as pd
+from oandapyV20 import API
+from oandapyV20.endpoints import accounts
 
 def main():
     config = configparser.ConfigParser()
     config.read('oanda.cfg')
 
     ACCOUNT_ID = config['oanda']['account_id']
-    #
-    # The config object is initialized by the argument parser, and contains
-    # the REST APID host, port, accountID, etc.
-    #
+    API_TOKEN = config['oanda']['account_token']
 
-    api = opy.APIv20(environment='practice',
-                access_token=config['oanda']['account_token'])
+    api = API(access_token=API_TOKEN)
 
-    instruments = api.account.get_instruments(ACCOUNT_ID)
-    print(instruments)
-    #
-    # Fetch the tradeable instruments for the Account found in the config file
-    #
-    # response = oanda.account..instruments(account_id)
+    req = accounts.AccountInstruments(ACCOUNT_ID)
+    res = api.request(req)
 
-    # #
-    # # Extract the list of Instruments from the response.
-    # #
-    # instruments = response.get("instruments", "200")
-
-    # instruments.sort(key=lambda i: i.name)
-
-    # def marginFmt(instrument):
-    #     return "{:.0f}:1 ({})".format(
-    #         1.0 / float(instrument.marginRate),
-    #         instrument.marginRate
-    #     )
-
-    # def pipFmt(instrument):
-    #     location = float(10 ** instrument.pipLocation)
-    #     return "{:.4f}".format (location)
-
-    # #
-    # # Print the details of the Account's tradeable instruments
-    # #
-    # common.view.print_collection(
-    #     "{} Instruments".format(len(instruments)),
-    #     instruments,
-    #     [
-    #         ("Name", lambda i: i.name),
-    #         ("Type", lambda i: i.type),
-    #         ("Pip", pipFmt),
-    #         ("Margin Rate", marginFmt),
-    #     ]
-    # )
-
+    instruments = [i['name'] for i in res['instruments']]
+    print(instruments, len(instruments))
 if __name__ == "__main__":
     main()
